73f5507a96619d6caf6366a896b41ab00264e75b
Bush <ulugbekbeshimov169@gmail.com>
2024-10-28 19:50:52 +0500

    New test main class

--- src/Chef.java
+++ src/Chef.java
@@ -8,8 +8,8 @@ public class Chef extends Employee implements AccountInt {
     private boolean loggedIn = false;
     private Account account;
 
-    public Chef(String name, String phone, String email, int employeeID, Date dateJoined, String position, double salary, String department, EmployeeStatus status, Account account) {
-        super(name, phone, email, employeeID, dateJoined, position, salary, department, status);
+    public Chef(String name, String phone, String email, int employeeID, Date dateJoined, String position, double salary, String department, EmployeeStatus status, Account account, Branch worksAt) {
+        super(name, phone, email, employeeID, dateJoined, position, salary, department, status, worksAt);
         this.account = account;
     }
 
---
+++ src/TestMain.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TestMain {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who do you want to log in as :");
+        System.out.println("1. Manager\n2. Cashier\n3. Waiter\n4. Chef\n5. Receptionist");
+    }
+}
